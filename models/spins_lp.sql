b9cebe72b349432cc3ba4848e8b5a72232efc908
Malik Redfox <malik@redfox.io>
2022-04-20 14:53:02 +0000

    converted denom into float

--- models/spins_lp.sql
+++ models/spins_lp.sql
@@ -192,6 +192,111 @@ select *
 	, cast(unit_sales_yago as float)/nullif(tdp_yago,0) as unit_sales_tdp_yago
 	, (cast(unit_sales as float)/nullif(tdp,0))/nullif((cast(unit_sales_2_yago as float)/nullif(tdp_2_yago,0)),0) -1 unit_sales_tdp_per_change_2_yago
 	, (cast(unit_sales as float)/nullif(tdp,0))/nullif((cast(unit_sales_yago as float)/nullif(tdp_yago,0)),0) -1 unit_sales_tdp_per_change_yago
+from basic_calculations
+), level_3 as (
+select *
+	, (per_sales_promo/nullif(per_sales_promo_2_yago,0))*100 as per_sales_promo_pt_chg_2_yago
+	, (per_sales_promo/nullif(per_sales_promo_yago,0))*100 as per_sales_promo_pt_chg_yago
+	, case when promo_arp < base_arp then ((promo_arp/nullif(base_arp,0))-1) ELSE 0 end per_discount_promo
+	, case when promo_arp_2_yago < base_arp_2_yago then ((promo_arp_2_yago/nullif(base_arp_2_yago,0))-1) ELSE 0 end per_discount_promo_2_yago
+	, case when promo_arp_yago < base_arp_yago then ((promo_arp_yago/nullif(base_arp_yago,0))-1) ELSE 0 end per_discount_promo_yago
+	, 1- per_promo_incrementality as per_promo_subsidized_base
+	, (per_unit_sales_promo- per_unit_sales_promo_2_yago)*100 as per_unit_sales_promo_pt_chg_2_yago
+	, (per_unit_sales_promo- per_unit_sales_promo_yago)*100 as per_unit_sales_promo_pt_chg_yago
+	, tdp_change_yago*(sales_non_promo_yago/nullif(tdp_yago,0))*0.7 as change_due_to_distribution
+	, tdp_change_2_yago*(sales_non_promo_2_yago/nullif(tdp_2_yago,0))*0.7 as change_due_to_distribution_2_yago
+	, base_arp- base_arp_2_yago as base_arp_pt_change_2_yago
+	, base_arp- base_arp_yago as base_arp_pt_change_yago
+	, (base_arp- base_arp_yago)*(base_unit_sales_yago) as change_due_to_pricing
+	, (base_arp- base_arp_2_yago)*(base_unit_sales_2_yago) as change_due_to_pricing_2_yago
+	, incremental_sales-incremental_sales_yago as change_due_to_promotion
+	, incremental_sales-incremental_sales_2_yago as change_due_to_promotion_2_yago
+	, promo_arp- base_arp as discount_promo
+	, promo_arp_2_yago- base_arp_2_yago as discount_promo_2_yago
+	, promo_arp_yago- base_arp_yago as discount_promo_yago
+	, (lift-lift_2_yago)*100 as lift_pt_change_2_yago
+	, (lift-lift_yago)*100 as lift_pt_change_yago
+	, sales_non_promo/nullif(sales_non_promo_2_yago,0) -1 as sales_non_promo_pt_change_2_yago
+	, sales_non_promo/nullif(sales_non_promo_yago,0) -1 as sales_non_promo_pt_change_yago
+	, sales_acv/nullif(sales_acv_2_yago,0) -1 as sales_acv_per_change_2_yago
+	, sales_acv/nullif(sales_acv_yago,0) -1 as sales_acv_per_change_yago
+	, stores_selling*avg_no_items as gdp 
+	, stores_selling_2_yago*avg_no_items_2_yago as gdp_2_yago
+	, stores_selling_yago*avg_no_items_yago as gdp_yago
+	, (stores_selling*avg_no_items)/nullif(stores_selling_2_yago*avg_no_items_2_yago,0) -1 gdp_per_change_2_yago
+	, (stores_selling*avg_no_items)/nullif(stores_selling_yago*avg_no_items_yago,0) -1 gdp_per_change_yago
+	, sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0) as sales_stores_weeks_avg_no_items
+	, sales_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0) as sales_stores_weeks_avg_no_items_2_yago
+	, sales_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0) as sales_stores_weeks_avg_no_items_yago
+	, (sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/
+		nullif(sales_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0),0) -1 as sales_stores_weeks_avg_no_items_per_change_2_yago
+	, (sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/
+		nullif(sales_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0),0) -1 as sales_stores_weeks_avg_no_items_per_change_yago
+	, annualized_sales/nullif(annualized_sales_2_yago,0) -1 as annualized_sales_per_change_2_yago
+	, annualized_sales/nullif(annualized_sales_yago,0) -1 as annualized_sales_per_change_yago
+	, annualized_sales/nullif(tdp,0) as annualized_sales_tdp
+	, annualized_sales_yago/nullif(tdp_yago,0) as annualized_sales_tdp_yago
+	, annualized_unit_sales/nullif(tdp,0) as annualized_unit_sales_tdp
+	, annualized_unit_sales_yago/nullif(tdp_yago,0) as annualized_unit_sales_tdp_yago
+	, annualized_unit_sales/nullif(annualized_unit_sales_2_yago,0) -1 as annualized_unit_sales_per_change_2_yago
+	, annualized_unit_sales/nullif(annualized_unit_sales_yago,0) -1 as annualized_unit_sales_per_change_yago
+	, arp/nullif(arp_yago,0) as arp_per_change_yago
+	, arp - arp_yago as arp_pt_change_yago
+	, arp - arp_2_yago as arp_pt_change_2_yago
+	, avg_no_items/nullif(avg_no_items_2_yago,0) -1 as avg_no_items_per_change_2_yago
+	, avg_no_items/nullif(avg_no_items_yago,0) -1 as avg_no_items_per_change_yago
+	, base_arp/nullif(base_arp_yago,0) -1 as base_arp_per_change_yago
+
+	, base_unit_sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0) as base_unit_sales_stores_weeks_avg_no_items
+	, base_unit_sales_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0) as base_unit_sales_stores_weeks_avg_no_items_2_yago
+	, base_unit_sales_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0) as base_unit_sales_stores_weeks_avg_no_items_yago
+	, (base_unit_sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/nullif(base_unit_sales_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0),0) -1 as base_unit_sales_stores_weeks_avg_no_items_per_change_2_yago
+	, (base_unit_sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/nullif(base_unit_sales_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0),0) -1 as base_unit_sales_stores_weeks_avg_no_items_per_change_yago	
+	, incremental_unit_sales/nullif(incremental_unit_sales_2_yago,0) -1 as incremental_unit_sales_per_change_2_yago
+	, incremental_unit_sales/nullif(incremental_unit_sales_yago,0) -1 as incremental_unit_sales_per_change_yago
+	, promo_arp/nullif(promo_arp_yago,0) -1 as promo_arp_per_change_yago
+	, promo_arp-promo_arp_2_yago as promo_arp_pt_change_2_yago
+	, promo_arp-promo_arp_yago as promo_arp_pt_change_yago
+	, unit_sales_promo/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0) as unit_sales_promo_stores_weeks_avg_no_items
+	, unit_sales_promo_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0) as unit_sales_promo_stores_weeks_avg_no_items_2_yago
+	, unit_sales_promo_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0) as unit_sales_promo_stores_weeks_avg_no_items_yago
+	, (unit_sales_promo/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/nullif(unit_sales_promo_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0),0) -1 as unit_sales_promo_stores_weeks_avg_no_items_per_change_2_yago
+	, (unit_sales_promo/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/nullif(unit_sales_promo_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0),0) -1 as unit_sales_promo_stores_weeks_avg_no_РЎС™ems_per_change_yago
+	, incremental_unit_sales/nullif(base_unit_sales_promo,0) as unit_lift
+	, incremental_unit_sales_2_yago/nullif(base_unit_sales_promo_2_yago,0) as unit_lift_2_yago
+	, incremental_unit_sales_yago/nullif(base_unit_sales_promo_yago,0) as unit_lift_yago
+	, ((incremental_unit_sales/nullif(base_unit_sales_promo,0))- (incremental_unit_sales_2_yago/nullif(base_unit_sales_promo_2_yago,0))	)*100 as unit_lift_pt_change_2_yago
+	, ((incremental_unit_sales/nullif(base_unit_sales_promo,0))- (incremental_unit_sales_yago/nullif(base_unit_sales_promo_yago,0))	)*100 as unit_lift_pt_change_yago
+	, unit_sales_promo/nullif(unit_sales_non_promo_2_yago,0) as unit_sales_non_promo_per_change_2_yago
+	, unit_sales_promo/nullif(unit_sales_non_promo_yago,0) as unit_sales_non_promo_per_change_yago
+	, unit_sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0) as unit_sales_stores_weeks_avg_no_items
+	, unit_sales_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0) as unit_sales_stores_weeks_avg_no_items_2_yago
+	, unit_sales_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0) as unit_sales_stores_weeks_avg_no_items_yago
+	, (unit_sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/nullif(unit_sales_2_yago/nullif(weeks_selling_2_yago,0)/nullif(stores_selling_2_yago,0)/nullif(avg_no_items_2_yago,0),0) unit_sales_stores_weeks_avg_no_items_per_change_2_yago_raw
+	, (unit_sales/nullif(weeks_selling,0)/nullif(stores_selling,0)/nullif(avg_no_items,0))
+		/nullif(unit_sales_yago/nullif(weeks_selling_yago,0)/nullif(stores_selling_yago,0)/nullif(avg_no_items_yago,0) ,0) unit_sales_stores_weeks_avg_no_items_per_change_yago_raw	
+	
+	, sales/nullif(unit_sales,0)/nullif(avg_size,0) as arp_per_oz
+	, sales_2_yago/nullif(unit_sales_2_yago,0)/nullif(avg_size,0) as arp_per_oz_2_yago
+	, sales_yago/nullif(unit_sales_yago,0)/nullif(avg_size,0) as arp_per_oz_yago
+	, sales/nullif(total_category_sales,0)*100 as dollar_share
+	, sales_2_yago/nullif(total_category_sales_2_yago,0)*100 as dollar_share_2_yago
+	, sales_yago/nullif(total_category_sales_yago,0)*100 as dollar_share_yago
+	, (sales/nullif(total_category_sales,0)*100) -(sales_2_yago/nullif(total_category_sales_2_yago,0)*100) dollar_share_pt_change_2_yago
+	, (sales/nullif(total_category_sales,0)*100) -(sales_yago/nullif(total_category_sales_yago,0)*100) dollar_share_pt_change_yago
+	, tdp/nullif(total_category_tdp,0)*100 as tdp_share
+	, cast(unit_sales as float)/nullif(total_category_unit_sales,0)*100 as unit_share
+	, unit_sales_2_yago/nullif(total_category_unit_sales_2_yago,0)*100 as unit_share_2_yago
+	, unit_sales_yago/nullif(total_category_unit_sales_yago,0)*100 as unit_share_yago
+	, (unit_sales/nullif(total_category_unit_sales,0)*100) -(unit_sales_2_yago/nullif(total_category_unit_sales_2_yago,0)*100) unit_share_pt_change_2_yago
+	, (unit_sales/nullif(total_category_unit_sales,0)*100) -(unit_sales_yago/nullif(total_category_unit_sales_yago,0)*100) unit_share_pt_change_yago
 from level_2
 )
 select *
